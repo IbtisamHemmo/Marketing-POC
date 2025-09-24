a3256f474350d4eed85a1ee8208933e60f17b774
ibtisamhemmo <ibtisam.hemmo@floranow.com>
2025-09-24 10:44:36 +0300

    fix: use env variable for API version

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,7 +5,7 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: '2024-01-01',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: false,
 })
 
